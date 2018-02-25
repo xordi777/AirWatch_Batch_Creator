8e80b74554afaef5644427727a6e352b04e220ce
xordi777 <xordi777@outlook.com>
2018-02-25 09:20:12 -0800

    check for monittor resoltion

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main extends Application {
 
@@ -15,12 +16,30 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("AirWatch Batch Creator");
         Scene scene = new Scene(root, 600, 275);
-        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
+        //Determine which stylesheet to load (depends on monitor resolution)
+        if(checkScreenSize()) {
+            scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        } else {
+            scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        }
+
         primaryStage.setScene(scene);
         primaryStage.show();
         primaryStage.sizeToScene();
     }
 
+    // Return true if the monitor resultion is higher than FHD.
+    private boolean checkScreenSize() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double width = screenSize.getWidth();
+        double height = screenSize.getHeight();
+
+        if(width > 1920) {
+            return true;
+        }
+        else { return false;}
+    }
 
     public static void main(String[] args) {
         launch(args);
